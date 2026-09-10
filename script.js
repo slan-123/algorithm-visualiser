ef38a23ced1094d81f336231bac9d89106783bea
slan-123 <arslan.raiees06@gmail.com>
2026-09-10 13:19:42 +0100

    Fix array value usage in generateArray function

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ const arrayContainer = document.getElementById("array-container");
 function generateArray() {
     arrayContainer.innerHTML = "";
 
+    const array = [];
+
     for (let i = 0; i < 30; i++) {
         const value = Math.floor(Math.random() * 100) + 1;
 
+        array.push(value);
+    }
+
+    for (let i = 0; i < array.length; i++) {
         const bar = document.createElement("div");
 
-        bar.style.height = value * 3 + "px";
+        bar.style.height = array[i] * 3 + "px";
 
         arrayContainer.appendChild(bar);
     }
+
+    console.log(array);
 }
 
 generateButton.addEventListener("click", generateArray);
